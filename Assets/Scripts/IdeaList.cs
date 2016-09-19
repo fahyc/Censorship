823173d626b0c4ab224f39c7f2d72ed4be297a60
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2016-09-19 15:57:58 -0400

    Need to keep track of global number of nodes and added method to update values for each AbstractIdea

--- Assets/Scripts/IdeaList.cs
+++ Assets/Scripts/IdeaList.cs
@@ -5,16 +5,22 @@ using System;
 class IdeaList : MonoBehaviour {
 
 	public AbstractIdea[] list;
+	public static int nodeCount = 0;
+
+	void Update() {
+		// TODO: check value of each AbstractIdea and fire an event if high enough
+	}
 }
 	
 [Serializable]
 class AbstractIdea : System.Object {
 	public string name;
 	public string description;
-
-	[SerializeField]
 	public int opposite;
-
-	public int value;
+	public float value;
 	public Color color;
+
+	public void updateValue(float amt) {
+		value = value + (amt / IdeaList.nodeCount);
+	}
 }
