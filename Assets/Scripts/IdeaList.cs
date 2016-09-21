44373c5c402a282830a2a084999858038def15cc
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2016-09-21 00:54:36 -0400

    Change event logic to check randomly every second wheither or not to fire an event

--- Assets/Scripts/IdeaList.cs
+++ Assets/Scripts/IdeaList.cs
@@ -5,24 +5,56 @@ using System.Collections.Generic;
 
 class IdeaList : MonoBehaviour {
 
-	public AbstractIdea[] list;
-	public static AbstractIdea[] staticList;
+    public AbstractIdea[] list;
+    public static AbstractIdea[] staticList;
 
-	public static int nodeCount = 0;
+    public static int nodeCount = 0;
 
-	public Dictionary<string, int> ideaDict = new Dictionary<string, int>();
-	public static Dictionary<string, int> staticDict;
+    public Dictionary<string, int> ideaDict = new Dictionary<string, int>();
+    public static Dictionary<string, int> staticDict;
 
-	void Awake()
-	{
-		staticList = list;
-		for (int i = 0; i < list.Length; i++)
-		{
-			ideaDict.Add(list[i].name, i);
-		}
+    static IdeaList instance;
 
-		staticDict = ideaDict;
-	}
+    void Awake()
+    {
+        instance = this;
+        staticList = list;
+        for (int i = 0; i < list.Length; i++)
+        {
+            ideaDict.Add(list[i].name, i);
+        }
+
+        staticDict = ideaDict;
+    }
+
+    public static void rollForEvent(AbstractIdea idea)
+    {
+        instance.StartCoroutine(instance.checkEvent(idea));
+    }
+
+    IEnumerator checkEvent(AbstractIdea idea)
+    {
+        int index = staticDict[idea.name];
+        float ratio = staticList[index].value / (float) nodeCount;
+
+        while (ratio > staticList[index].getEventThreshold())
+        {
+            Debug.Log(idea.name + ": " + ratio);
+
+            if (UnityEngine.Random.value < 0.05)
+            {
+                idea.triggerEvent();
+                break;
+            }
+
+            yield return new WaitForSeconds(1);
+
+            ratio = staticList[index].value / (float) nodeCount;
+        }
+
+        idea.stopChecking();
+        yield return null;
+    }
 }
 	
 [Serializable]
@@ -34,6 +66,7 @@ class AbstractIdea : System.Object {
 	public Color color;
     public Event[] events;
     int eventIndex = 0;
+    bool checkingForEvents = false;
 
     [Serializable]
     public struct Event
@@ -46,16 +79,29 @@ class AbstractIdea : System.Object {
     public void updateValue(int amt) {
         value += amt;
 
-        float ratio = (float) value / (float) IdeaList.nodeCount;
-
-        if (eventIndex < events.Length && ratio > events[eventIndex].threshold)
+        if (eventIndex < events.Length && !checkingForEvents)
         {
-            // trigger event and display description
-            Global.text = "<size=16><b><color=#" + ColorToHex(color) + ">" + events[eventIndex].name + "</color></b></size>" + ": " + events[eventIndex].description + "\n\n";
-            eventIndex++;
+            checkingForEvents = true;
+            IdeaList.rollForEvent(this);
         }
-
 	}
+
+    public void stopChecking()
+    {
+        checkingForEvents = false;
+    }
+
+    public void triggerEvent()
+    {
+        // trigger event and display description
+        Global.text = "<size=16><b><color=#" + ColorToHex(color) + ">" + events[eventIndex].name + "</color></b></size>" + ": " + events[eventIndex].description + "\n\n";
+        eventIndex++;
+    }
+
+    public float getEventThreshold()
+    {
+        return events[eventIndex].threshold;
+    }
 
     string ColorToHex(Color32 color)
     {
