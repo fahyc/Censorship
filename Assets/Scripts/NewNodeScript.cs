a587378c47f13c860c54770557ed8c479afd2ee8
Nanyou Guan <nanyouguan@gmail.com>
2016-10-03 00:26:50 -0400

    working on node and group scripts

---
+++ Assets/Scripts/NewNodeScript.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NewNodeScript : MonoBehaviour {
+
+    float[] ideas;
+    int numIdeas;
+    float connectionRange;
+    float deviation;
+    int connectionRule;
+    int primaryIdea;
+    float ideaMin = 0.0f;
+    float ideaMax = 25.0f;
+    float primaryIdeaMin = 15.0f;
+    Collider2D[] nearbyNodes;
+    List<Node> connectedNodes;
+    NodeGroupScript parentGroup;
+    //group objects in charge of spawning initial nodes within group in circular formation, number of nodes spawned in group is up to designer
+
+    /**
+     * use spatial connections instead of color connections
+     * 
+     * blending rules:
+     * a = color1
+     * b = color2
+     * t = number between 0-1 representing point in blend between a and b
+     * blend r,g,b values
+     * sqrt((1-t)*pow(a, 2) + t * pow(b, 2))
+     * 
+     * connection rules using color wheel:
+     * -complementary = use opposite color
+     * -monochromatic = use 3 different values of same color
+     * -analogous = use 3 adjacent colors
+     * -split complements = use a color and it's two adjacent tertiary colors of its compliment
+     * -triadic = use 3 evenly spaced colors
+     * -tetradic = use 2 complementary pairs
+     * -group node groups based on connection rule to minimize cross network connections or specify connection range
+     * 
+     * generation rules:
+     * -manual initial placement
+     * 
+     * growth rules:
+     * -within a node group once a small group of nodes with colors outside of allowed range is generated separate into new node group
+     * -if two or more node groups are converted into color within allowed range combine into larger node group while also combining connections
+     * **/
+
+    // Use this for initialization
+    void Start () {
+        for(int i = 0; i < 10; i++)
+        {
+            //generate variation based on primary idea
+            if(i == primaryIdea)
+            {
+                ideas[i] = Random.Range(primaryIdeaMin, ideaMax);
+            }
+            else
+            {
+                ideas[i] = Random.Range(ideaMin, ideaMax);
+            }
+        }
+        connectionRule = Random.Range(0, 6);
+        connectionRange = Random.Range(1.0f, 5.0f);
+        deviation = 25.0f;
+        numIdeas = ideas.Length;
+        nearbyNodes = Physics2D.OverlapCircleAll(transform.position, connectionRange);
+        //generate connections by searching in range and following rule
+        switch(connectionRule)
+        {
+            case 0:
+                complemetary();
+                break;
+            case 1:
+                monochromatic();
+                break;
+            case 2:
+                analogous();
+                break;
+            case 3:
+                splitComplements();
+                break;
+            case 4:
+                triadic();
+                break;
+            case 5:
+                tetradic();
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void complemetary()
+    {
+        //since nodes are spawned in circular formation connect with nodes based on location, use circular degrees/angles and distance to determine connections
+        //complementary = 1 connection, 180 degree, max distance
+    }
+
+    void monochromatic()
+    {
+        //monochromatic = 2 connections, 180 degree, 1/3 and 2/3 distance
+    }
+
+    void analogous()
+    {
+        //analogous = 2 connections, first node clockwise and counterclockwise
+    }
+
+    void splitComplements()
+    {
+        //split complements = 2 connections, first node 150 degrees clockwise and counterclockwise
+    }
+
+    void triadic()
+    {
+        //triadic = 2 connections, first node 120 degrees clockwise and counterclockwise
+    }
+
+    void tetradic()
+    {
+        //tetradic = "square connection", 90 degrees clockwise and cunterclockwise between all 4 nodes
+    }
+}
---
+++ Assets/Scripts/NodeGroupScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeGroupScript : MonoBehaviour {
+
+    int maxNodes = 15;
+    int minNodes = 5;
+    int numNodes;
+    Node referenceNode;
+    //make group connections pre-determined (the same everytime level is loaded)
+    //connections within a group will be procedural but connections between groups will not
+    //for group connections make a variable number of connections and get list of groups within certain range and distribute variable number of connections amongst them
+
+	// Use this for initialization
+	void Start () {
+        numNodes = Random.Range(minNodes, maxNodes);
+        //spawn nodes in circular formation
+        //calculate radius based on number of nodes
+        float radius = (numNodes * referenceNode.GetComponent<SpriteRenderer>().sprite.rect.width) / 2;
+
+        for(int i = 0; i < numNodes; i++)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
