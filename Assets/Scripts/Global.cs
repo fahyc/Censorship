15babebb0335ac0ff2cd9c5da71fe18b018f0551
Connor Fahy <heskey333@gmail.com>
2016-10-19 14:07:52 -0400

    Fixed a couple of bugs caused by multiple global objects being in each scene if there are multiple players.

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -24,7 +24,7 @@ public class Global : NetworkBehaviour {
 
     public Inspect inspectCanvas;
 
-    DummyUnit activeDummy;
+    public DummyUnit activeDummy;
 
     public int startingMoney = 500;
     public int currentMoney = 0;
@@ -32,8 +32,18 @@ public class Global : NetworkBehaviour {
 
     public int moneyDiff;
     public bool broke = false;
+
+	void Start()
+	{
+		if (!isLocalPlayer && !isServer)
+		{
+			print("Is not local. becoming inactive");
+			gameObject.SetActive(false);
+		}
+	}
     // Use this for initialization
     public override void OnStartLocalPlayer () {
+		
 		inspector = GameObject.FindGameObjectWithTag("Inspector").GetComponent<Inspect>();
 		//infoTextBox = GameObject.FindGameObjectWithTag("")
         currentMoney = startingMoney;
@@ -79,6 +89,11 @@ public class Global : NetworkBehaviour {
     [Client]
     void SpawnObj(Spawnable prefabObject, Vector3 pos, int index)
     {
+		if (!isLocalPlayer)
+		{
+			//Destroy(gameObject);
+			return;
+		}
         Spawnable costOfUnit = prefabObject.GetComponent<Spawnable>();
         //Do we have money to spawn this wall? If not, just quit. Also, we should probably display "No money to build" somewhere in the UI.
         if (currentMoney < costOfUnit.initialCost)
@@ -123,15 +138,34 @@ public class Global : NetworkBehaviour {
 			{//spawn whatever is selected
 				Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition.append(Camera.main.transform.position.z * -1));
                 SpawnObj(currentTool, position, toolIndex);
+				print("Current tool");
 			}
 			else {
 				//or if there is nothing to spawn, clear any focus and ui elements, or inspect whatever is below the mouse.
+				
 				Vector3 mousePos = Input.mousePosition;
 				mousePos.Set(mousePos.x, mousePos.y, -Camera.main.transform.position.z);
 				Collider2D[] hits = Physics2D.OverlapPointAll(Camera.main.ScreenToWorldPoint(mousePos));
-
-				if (hits.Length == 0)
+				print("trying clear ui.");
+				bool hit = false;
+				for (int i = 0; i < hits.Length; i++)
+				{//try inspecting something
+					Inspectable temp = hits[i].GetComponent<Inspectable>();
+					// Make sure we have authority on the object we're looking at
+					if (temp && temp.hasAuthority)
+					{
+						// print("enabling Inspect");
+						inspector.Enable(temp.gameObject);
+						hit = true;
+					}
+					else if (hits[i].GetComponent<Inspect>())
+					{
+						hit = true;
+					}
+				}
+				if (!hit)
 				{//Clear the UI if there is nothing below the mouse.
+					print("clearing UI.");
 					Global.text = "";
 					textbg = false;
 					for(int i = 0; i < toHide.Count; i++)
@@ -140,29 +174,6 @@ public class Global : NetworkBehaviour {
 					}
 					toHide.Clear();
 					DisableDummy();
-				}
-				else
-				{//otherwise try inspecting somehting. 
-					bool hit = false;
-					for(int i = 0; i < hits.Length; i++)
-					{
-						Inspectable temp = hits[i].GetComponent<Inspectable>();
-                        // Make sure we have authority on the object we're looking at
-						if (temp && temp.hasAuthority)
-						{
-							// print("enabling Inspect");
-							inspector.Enable(temp.gameObject);
-							hit = true;
-						}
-						else if (hits[i].GetComponent<Inspect>())
-						{
-							hit = true;
-						}
-					}
-					if (!hit)
-					{
-						inspector.Disable();
-					}
 				}
 			}
             //Global.text = "";
@@ -207,8 +218,12 @@ public class Global : NetworkBehaviour {
 
 	public void DisableDummy()
 	{
-        if (activeDummy != null)
-            Destroy(activeDummy.gameObject);
+		//print("disabling dummy" + activeDummy);
+		//print(activeDummy);
+		if (activeDummy != null)
+		{
+			Destroy(activeDummy.gameObject);
+		}
 	}
 
 	[Client]
