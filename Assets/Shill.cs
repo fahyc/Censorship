ee4ef01b1116b0703bbf891af9a6c184611697a5
Connor Fahy <heskey333@gmail.com>
2016-10-19 12:38:12 -0400

    Added code to make shills look like shills on the client side only.

--- Assets/Shill.cs
+++ Assets/Shill.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class Shill : Spawnable
 {
 	Node node;
+
+	public Sprite sprite;
 	// Use this for initialization
 	void Start () {
+
 		node = GetComponent<Node>();
 		for(int i = 0; i < DummyNode.nodes.Count; i++)
 		{
 			node.clientLinkTo(DummyNode.nodes[i]);
 		}
 		float[] strengths = new float[IdeaList.staticList.Length];
-
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		renderer.sprite = sprite;
 		//print(node.ideaStrengths);
 		strengths[index] = 1;
         node.shill = true;
