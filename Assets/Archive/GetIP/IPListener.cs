295bec2bb1c2008d8febf15f9d4a334bb46fe922
Connor Fahy <heskey333@gmail.com>
2016-11-18 19:53:08 -0500

    Server is working, in Archive/GetIP

---
+++ Assets/Archive/GetIP/IPListener.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IPListener{
+	void getData(string data);
+}
---
+++ Assets/Archive/GetIP/ManualListener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManualListener : MonoBehaviour,IPListener {
+	public AWSTest aws;
+
+	[System.Serializable]
+	public class SendData{
+		public string ip;
+	}
+	public void getData(string str)
+	{
+		print(str);
+	}
+	public void request()
+	{
+		aws.RequestUpdate(this);
+	}
+	public void send()
+	{
+		SendData t = new SendData();
+		t.ip = Network.player.ipAddress;
+		aws.SendUpdate(JsonUtility.ToJson(t));
+	}
+}
