4ac4469a216e2db7181b7da9b433fad00bde1549
Nanyou <guann@rpi.edu>
2016-11-30 17:54:38 -0500

    continued work on tutorial

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -20,9 +20,10 @@ public class TutorialScript : MonoBehaviour {
         //TODO: create central button/text listing mouse/keyboard controls
         //TODO: explain unit movement, firing, and fog of war
         promptText.text = text;
-        text = "Select a large nodes whose idea is the same as yours and choose the \"place office\" action.\n\n" +
+        text = "Select a large node whose idea is the same as yours and choose the \"place office\" action.\n\n" +
             "Placing an office will allow you to be able to purchase/hire other units.\n\n" +
-            "The ideas of large nodes are influenced by the nodes surrounding it; it will take on the most prevalant idea.";
+            "The large node and the nodes surrounding it represents a cluster.\n" +
+            "The large node is influenced by the ideas in the cluster and will take on the most prevalant idea.";
         //introduce office
         //introduce lurkers
         //introduce shills
@@ -43,7 +44,31 @@ public class TutorialScript : MonoBehaviour {
 
     void introduceOffice()
     {
-        text = "Select an office node whose idea is the same as yours and choose the \"place office\" action.\n";
+        text = "Office\n" +
+            "\tAllows you to purchase/hire other units.\n" +
+            "\tHas a bar above it that indicates its health.\n" +
+            "\tLooses health when ever a different idea becomes the majority it's cluster.\n" +
+            "\tCannot move and does not require upkeep.";
+        openPrompt();
+    }
+    
+    void introduceLurkers()
+    {
+        text = "Lurkers\n" +
+            "\tLift the fog of war and provide permanent vision.\n" +
+            "\tCan only be placed around the office hiring it.\n" +
+            "\tCannot be fired nor destroyed by the enemey.\n" +
+            "\tCannot move and does not require upkeep.";
+        openPrompt();
+    }
+
+    void introduceShills()
+    {
+        text = "Shills\n" +
+            "\tPeriodically send out a specific idea to any nodes its connected to.\n" +
+            "\tCan only be placed around the office hiring it." +
+            "\tCan be fired or destroyed by enemy hackers and botnets.\n" +
+            "\tCannot move and does not require upkeep.";
         openPrompt();
     }
 }
