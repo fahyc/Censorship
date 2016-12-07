a7785f22112386c994d5b82c6c4efbf46b54e305
Farkinator <thefarkinator@gmail.com>
2016-12-06 23:02:32 -0500

    Walls now have radii to indicate where their blocking is in effect.

--- Assets/Scripts/blockScript.cs
+++ Assets/Scripts/blockScript.cs
@@ -7,13 +7,21 @@ public class blockScript : Spawnable {
     [SyncVar]
 	public string sendsOut;//what idea this sends 
 	public Idea ideaTemplate;
-	
+    public SpriteRenderer aoeIndicator;
 
 	// Use this for initialization
 	void Start () {
 		if (index != -1) {
 			GetComponent<SpriteRenderer>().color = IdeaList.instance.list[index].color;
-		}
+            aoeIndicator.color = new Color(
+                IdeaList.instance.list[index].color.r,
+                IdeaList.instance.list[index].color.g,
+                IdeaList.instance.list[index].color.b,
+                0.2f
+            );
+            
+            //GetComponentInChildren<SpriteRenderer>().color = IdeaList.instance.list[index].color;
+ 		}
 	}
 	
 
